63f09b2834f920f22f2779ccb967cb96a4f7c554
RAHUL AGARWAL <rahulchamp2000@gmail.com>
2021-10-14 15:38:59 +0530

    Combined contracts

---
+++ src/contracts/Primary.sol
@@ -0,0 +1,115 @@
+pragma solidity ^0.5.16;
+
+contract Primary {
+    uint256 public videoCount = 0;
+    uint256 public usersCount = 0;
+    string public name = "Primary";
+    mapping(uint256 => Video) public Id_Video;
+    mapping(string => Video[]) public Tags_Videos;
+    mapping(string => Video) public Title_Video;
+    mapping(string => Video) public User_Videos;
+    mapping(string => User) public PublicKey_User;
+
+    struct Video {
+        uint256 id;
+        string hash;
+        string title;
+        string time;
+        string description;
+        string[] comments;
+        uint256 views;
+        uint256 hearts;
+        string tags;
+        string creatorId;
+    }
+
+    struct User {
+        string publicKey;
+        string userName;
+        string[] subscriptions;
+        uint256 subscribersCount;
+        mapping(string => Video[]) Playlist_Videos;
+    }
+
+    event VideoUploaded(uint256 id, string hash, string title, address author);
+
+    constructor() public {}
+
+    function signup(string memory _userName, string memory _publicKey) public {
+        usersCount++;
+        string[] subscriptions = [];
+        mapping(string => Video[]) Playlist_Videos;
+        PublicKey_User[_publicKey] = User(
+            _publicKey,
+            _userName,
+            subscriptions,
+            0,
+            Playlist_Videos
+        );
+    }
+
+    function transactions(
+        string[] memory _type,
+        string[] memory _userId,
+        string[] memory _timestamp,
+        string[][] memory _params
+    ) {
+        for (
+            uint256 transaction = 0;
+            transaction < _type.length;
+            transaction++
+        ) {
+            if(keccak256(abi.encodePacked(_type[transaction]))==abi.encodePacked("view")){
+                view(_userId[transaction],_params[transaction][0]);
+            }
+            else if(keccak256(abi.encodePacked(_type[transaction]))==abi.encodePacked("like")){
+                like(_userId[transaction],_params[transaction][0]);
+            }
+            else if(keccak256(abi.encodePacked(_type[transaction]))==abi.encodePacked("comment")){
+                comment(_userId[transaction],_params[transaction][0],_params[transaction][1]);
+            }
+            else if(keccak256(abi.encodePacked(_type[transaction]))==abi.encodePacked("subscribe")){
+                subscribe(_userId[transaction],_params[transaction][0])
+            }
+            else if(keccak256(abi.encodePacked(_type[transaction]))==abi.encodePacked("upload")){
+                uploadVideo(_params[transaction]][3], _params[transaction]][0], _timestamp[transaction], _params[transaction]][1], _params[transaction][2], _userId[transaction]);
+            }
+        }
+    }
+
+    function view(string memory _userId, string memory _videoId) public {
+        Id_Video[_videoId].views++;
+        PublicKey_User[_userId].Playlist_Videos["viewed"].push(_videoId);
+    }
+
+    function like(string memory _userId, string memory _videoId) public {
+        Id_Video[_videoId].hearts++;
+        PublicKey_User[_userId].Playlist_Videos["liked"].push(_videoId);
+    }
+
+    function comment(string memory _userId, string memory _videoId, string memory _comment) public {
+        Id_Video[_videoId].comments.push(_comment);
+    }
+
+    function subscribe(string memory _userId, string memory _subscribedUserId){
+        PublicKey_User[_userId].subscriptions.push(_subscribedUserId);
+        PublicKey_User[_subscribedUserId].subscribersCount++;
+    }
+
+    function uploadVideo(string memory _hash, string memory _title, string memory _time, string memory _description, string _tags, string _creatorId) public {
+        videoCount++;
+        string[] comments;
+        Id_Video[videoCount]=Video(
+            videoCount,
+            _hash,
+            _title,
+            _time,
+            _description,
+            comments,
+            0,
+            0,
+            _tags,
+            _creatorId
+        )
+    }
+}
